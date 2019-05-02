33cfe20178143148f3138e34c616db9049e7d0c8
NaskoVasilev <nasko_vasilev@abv.bg>
2019-05-02 22:08:13 +0300

    Scaffold Identity .NET Core 2.2

--- FunApp/FunApp.Web/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
+++ FunApp/FunApp.Web/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
@@ -94,6 +94,6 @@ namespace FunApp.Web.Areas.Identity.Pages.Account
                 ModelState.AddModelError(string.Empty, "Invalid authenticator code.");
                 return Page();
             }
-        }  
+        }
     }
 }
--- FunApp/FunApp.Web/Areas/Identity/Pages/Account/LoginWithRecoveryCode.cshtml.cs
+++ FunApp/FunApp.Web/Areas/Identity/Pages/Account/LoginWithRecoveryCode.cshtml.cs
@@ -68,7 +68,7 @@ namespace FunApp.Web.Areas.Identity.Pages.Account
             var recoveryCode = Input.RecoveryCode.Replace(" ", string.Empty);
 
             var result = await _signInManager.TwoFactorRecoveryCodeSignInAsync(recoveryCode);
-                
+
             if (result.Succeeded)
             {
                 _logger.LogInformation("User with ID '{UserId}' logged in with a recovery code.", user.Id);
