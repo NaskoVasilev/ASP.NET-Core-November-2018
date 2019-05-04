1c6c8dcd3fcbd0608e0bfbeaf7e06b8741d50a42
NaskoVasilev <nasko_vasilev@abv.bg>
2019-05-04 16:26:43 +0300

    Real time chat using SingalR and ASP.NET Core 2.2!

---
+++ SingalR/Chat/Chat/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+       public async Task SendMessage(string message)
+        {
+            string user = this.Context.User.Identity.Name;
+            await this.Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ SingalR/Chat/Chat/wwwroot/js/chat.js
@@ -0,0 +1,25 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chat").build();
+
+// The Receive Message Client event. This will trigger, when the Back-End calls the ReceiveMessage method
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = "[" + user + "]: " + msg;
+
+    var messageHtmlElement = $(`<div>${encodedMsg}</div>`);
+    var messageList = $("#messagesList");
+    messageList.append(messageHtmlElement);
+});
+
+//An error handler for connection errors
+connection.start().catch(function (err) {
+    return console.error(err.toString());
+});
+
+// The Send Message DOM event. This will trigger the Back-End SendMessage method
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var message = $("#messageInput").val();
+    connection.invoke("SendMessage", message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
